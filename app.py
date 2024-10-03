d58fee53c7ddcbfe6ecd33755d7d918c0e183068
Parth Sapar <43803157+saparparth@users.noreply.github.com>
2024-10-03 16:03:40 +0000

    usedpath appender to fix problem

--- app.py
+++ app.py
@@ -10,10 +10,27 @@ nltk.download('wordnet')
 nltk.download('omw-1.4')
 
 
-nltk.data.path.append('/home/vscode/nltk_data')
+# nltk.data.path.append('/home/vscode/nltk_data')
 # corpora/stopwords
 # /usr/local/bin/python /workspaces/Spam-Message-Detector/download_stopwords.py
 # venv/lib/python3.11/site-packages/nltk
+import nltk
+from nltk.corpus import stopwords
+
+# Set the nltk_data path to a local folder inside the app
+nltk_data_path = './nltk_data'
+nltk.data.path.append(nltk_data_path)
+
+# Check if stopwords are downloaded, if not, download them
+# try:
+#     stopwords.words('english')
+# except LookupError:
+#     st.info("Downloading NLTK stopwords...")
+#     nltk.download('stopwords', download_dir=nltk_data_path)
+
+# # Your normal Streamlit code continues here
+# st.write("Stopwords loaded successfully!")
+
 ps = PorterStemmer()
 
 
--- download_stopwords.py
+++ download_stopwords.py
@@ -1,2 +1,17 @@
+import streamlit as st
+import nltk
 from nltk.corpus import stopwords
-print(stopwords.words('english')[:10])
+
+# Set the nltk_data path to a local folder inside the app
+nltk_data_path = './nltk_data'
+nltk.data.path.append(nltk_data_path)
+
+# Check if stopwords are downloaded, if not, download them
+try:
+    stopwords.words('english')
+except LookupError:
+    st.info("Downloading NLTK stopwords...")
+    nltk.download('stopwords', download_dir=nltk_data_path)
+
+# Your normal Streamlit code continues here
+st.write("Stopwords loaded successfully!")
